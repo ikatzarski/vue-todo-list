9406fec7aa8782bc26d5ad190e33e7b35a0d5c43
Ivan Katzarski <10603133+ikatzarski@users.noreply.github.com>
2020-12-27 15:43:51 +0000

    Add TodoItem component

---
+++ src/components/TodoItem.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="todo-item">
+    <p>{{ todo.title }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TodoItem",
+  props: ["todo"],
+};
+</script>
+
+<style scoped>
+.todo-item {
+  background: #f4f4f4;
+  padding: 10px;
+  border-bottom: 1px #ccc dotted;
+}
+</style>
