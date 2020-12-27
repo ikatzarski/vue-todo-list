9a4e881d049fcc401a3d6ee0b724376733ff137b
Ivan Katzarski <10603133+ikatzarski@users.noreply.github.com>
2020-12-27 16:07:25 +0000

    Emit an event from TodoItem to App to remove a Todo

--- src/components/TodoItem.vue
+++ src/components/TodoItem.vue
@@ -3,6 +3,7 @@
     <p>
       <input type="checkbox" v-on:change="markComplete" />
       {{ todo.title }}
+      <button @click="$emit('del-todo', todo.id)" class="del">x</button>
     </p>
   </div>
 </template>
@@ -28,5 +29,15 @@ export default {
 
 .is-complete {
   text-decoration: line-through;
+}
+
+.del {
+  background: #ff0000;
+  color: #fff;
+  border: none;
+  padding: 5px 9px;
+  border-radius: 50%;
+  cursor: pointer;
+  float: right;
 }
 </style>
