2eb2ae1e688c7d166b4710636127e8cbaead0f82
Ivan Katzarski <10603133+ikatzarski@users.noreply.github.com>
2020-12-31 00:59:30 +0000

    Add jest and vue-test-utils and all of their necessary config

---
+++ jest.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  verbose: true,
+  roots: ["<rootDir>/src/"],
+  moduleFileExtensions: ["js", "json", "vue"],
+  transform: {
+    ".*\\.(vue)$": "vue-jest",
+    ".*\\.(js)$": "babel-jest",
+  },
+  moduleNameMapper: {
+    "^@/(.*)$": "<rootDir>/src/$1",
+  },
+  collectCoverage: true,
+  collectCoverageFrom: ["**/*.{js,vue}", "!**/node_modules/**"],
+};
