9492fc4a815095a626686be2679608b0999ed9ca
Ivan Katzarski <10603133+ikatzarski@users.noreply.github.com>
2021-01-03 18:20:02 +0000

    Add tests for the markComplete function in TodoItem

--- src/components/TodoItem.test.js
+++ src/components/TodoItem.test.js
@@ -2,35 +2,55 @@ import { mount } from "@vue/test-utils";
 import TodoItem from "./TodoItem";
 
 describe("TodoItem", () => {
-  it("should mark an incomplete todo as incomplete", () => {
-    const testTodo = {
-      id: 1,
-      title: "Eat many cookies",
-      completed: false,
-    };
+  const testTodo = (isComplete) => ({
+    id: 1,
+    title: "Eat many cookies",
+    completed: isComplete,
+  });
 
+  it("should not cross out an incomplete todo", () => {
     const wrapper = mount(TodoItem, {
       propsData: {
-        todo: testTodo,
+        todo: testTodo(false),
       },
     });
 
     expect(wrapper.find("p").element.className).toBe("");
   });
 
-  it("should mark a complete todo as complete", () => {
-    const testTodo = {
-      id: 1,
-      title: "Eat many cookies",
-      completed: true,
-    };
-
+  it("should should cross out a complete todo", () => {
     const wrapper = mount(TodoItem, {
       propsData: {
-        todo: testTodo,
+        todo: testTodo(true),
       },
     });
 
     expect(wrapper.find("p").element.className).toBe("is-complete");
+  });
+
+  it("should mark an incomplete todo as complete", async () => {
+    const wrapper = mount(TodoItem, {
+      propsData: {
+        todo: testTodo(false),
+      },
+    });
+
+    const checkbox = wrapper.find("input[type='checkbox']");
+    await checkbox.setChecked();
+
+    expect(wrapper.props().todo.completed).toBeTruthy();
+  });
+
+  it("should mark a complete todo as incomplete", async () => {
+    const wrapper = mount(TodoItem, {
+      propsData: {
+        todo: testTodo(true),
+      },
+    });
+
+    const checkbox = wrapper.find("input[type='checkbox']");
+    await checkbox.setChecked();
+
+    expect(wrapper.props().todo.completed).toBeFalsy();
   });
 });
